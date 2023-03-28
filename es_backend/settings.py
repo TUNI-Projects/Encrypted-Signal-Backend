dc431ae00f055781c8606766bd3c87a3c2d4337c
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-03-28 22:56:03 +0300

    base user structure added

--- es_backend/settings.py
+++ es_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import sys
 from pathlib import Path
 from decouple import config
+ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # -----------------------
+    'user.apps.UserConfig',
+    'share.apps.ShareConfig',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom user model
+AUTH_USER_MODEL = 'user.User'
