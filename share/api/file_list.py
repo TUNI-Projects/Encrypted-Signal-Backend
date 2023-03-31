b82bdfd1aa6c8f383d15f01cad134155bfd9d4bd
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-03-31 19:01:38 +0300

    file list and download api added

---
+++ share/api/file_list.py
@@ -0,0 +1,38 @@
+from rest_framework.views import APIView
+from django.http import JsonResponse
+from user.models import User
+from share.models import FileModel
+
+class ListOfFiles(APIView):
+    
+    REQURIED_PARAMs = ("username", )
+    
+    def get(self, request):
+        data = request.data
+        
+        if "username" not in data:
+            return JsonResponse({
+                "message": "Missing parameter `username` is missing! Invalid Request"
+            }, status=400)
+        
+        try:
+            user_obj = User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+            return JsonResponse({
+                "message": "Invalid request",
+            }, status=404)
+        
+        list_of_files = list(FileModel.objects.filter(file_owner = user_obj.pk))
+        response = []
+        
+        for uploaded_file in list_of_files:
+            response.append({
+                "original_filename": uploaded_file.original_filename,
+                "file_path": uploaded_file.file.name,
+                "uploaded_at": uploaded_file.uploaded_at,
+            })
+        
+        return JsonResponse({
+            "total": len(list_of_files),
+            "files": response,
+        }, status=200)
--- share/urls.py
+++ share/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from share.api.file_upload import FileUploadAPI
+from share.api.file_list import ListOfFiles
 
 urlpatterns = [
     path('upload/', FileUploadAPI.as_view()),
+    path('uploaded_files/', ListOfFiles.as_view()),
 ]
